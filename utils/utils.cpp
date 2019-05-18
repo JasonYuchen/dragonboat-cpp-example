9e5f1ea236d54db5bcb4c9bf4bcc3ddd2c4c9ac7
JASONWORK\jason <jasonyuchen@foxmail.com>
2019-05-18 21:46:24 +0800

    revert

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <algorithm>
-#include <type_traits>
 #include "utils.h"
 
 std::vector<std::string> split(const std::string &cmd, const char &delim)
--- utils/utils.h
+++ utils/utils.h
@@ -10,13 +10,5 @@
 
 std::vector<std::string> split(const std::string &cmd, const char &delim = ' ');
 
-template<typename Iter>
-void printAll(std::ostream &out, Iter begin, Iter end)
-{
-  static_assert(
-    std::is_base_of<std::forward_iterator_tag,
-                    typename Iter::iterator_category>::value,
-    "printAll only accept iterator");
-}
 
 #endif //DRAGONBOAT_CPP_EXAMPLE_UTILS_UTILS_H_
