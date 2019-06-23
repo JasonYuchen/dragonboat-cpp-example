5783ffecda9aae7fca68eaacdac6d55aca3ecbb0
jasonyuchen <jasonyuchen@foxmail.com>
2019-06-23 23:26:04 +0800

    ondisk: enable more functions

--- ondisk/main.cpp
+++ ondisk/main.cpp
@@ -15,16 +15,21 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cassert>
 #include <getopt.h>
 #include <dragonboat/dragonboat.h>
+#include "zupply.hpp"
 #include "statemachine.h"
-#include "utils.h"
 
 constexpr uint64_t ClusterID = 128;
 
 enum RequestType : int {
-  PUT = 0,
-  GET = 1,
+  EXIT = 0,
+  PUT = 1,
+  GET = 2,
+  ADD_NODE = 3,
+  REMOVE_NODE = 4,
+  UNKNOWN,
 };
 
 const std::string addresses[] = {
@@ -42,9 +47,29 @@ void printUsage()
     << "exit" << std::endl;
 }
 
+std::tuple<RequestType, std::string, std::string> parseRequest(std::string &msg)
+{
+  auto parts = zz::fmt::split(msg);
+  if (parts.empty() || parts.size() > 3) {
+    return {UNKNOWN, "", ""};
+  } else if (zz::fmt::to_lower_ascii(parts[0]) == "exit") {
+    return {EXIT, "", ""};
+  } else if (zz::fmt::to_lower_ascii(parts[0]) == "put") {
+    assert(parts.size() == 3);
+    return {PUT, std::move(parts[1]), std::move(parts[2])};
+  } else if (zz::fmt::to_lower_ascii(parts[0]) == "get") {
+    return {GET, std::move(parts[1]), ""};
+  } else if (zz::fmt::to_lower_ascii(parts[0]) == "add") {
+    return {ADD_NODE, std::move(parts[1]), std::move(parts[2])};
+  } else if (zz::fmt::to_lower_ascii(parts[0]) == "remove") {
+    return {REMOVE_NODE, std::move(parts[1]), ""};
+  } else {
+    return {UNKNOWN, "", ""};
+  }
+}
+
 int main(int argc, char **argv, char **env)
 {
-  // TODO: use getopt
   int ret;
   uint64_t nodeID = 0;
   bool join = false;
@@ -52,18 +77,28 @@ int main(int argc, char **argv, char **env)
   // for simplicity, membership change is removed in this example
   struct ::option opts[] = {
     {"nodeid", required_argument, nullptr, 0},
+    {"addr", required_argument, nullptr, 1},
+    {"join", no_argument, nullptr, 2},
   };
 
   while ((ret = getopt_long_only(argc, argv, "", opts, nullptr)) != -1) {
     switch (ret) {
       case 0:nodeID = std::stoull(optarg);
         break;
+      case 1:address = optarg;
+        break;
+      case 2:join = true;
+        break;
       default:std::cerr << "unknown ret " << ret << std::endl;
+        break;
     }
   }
 
-  if (nodeID < 1 || nodeID > 3) {
+  if (nodeID < 1) {
     std::cerr << "invalid node id: " << nodeID << std::endl;
+    return -1;
+  } else if (nodeID > 3 && address.empty()) {
+    std::cerr << "undefined node address" << std::endl;
     return -1;
   } else if (nodeID <= 3) {
     address = addresses[nodeID - 1];
@@ -82,7 +117,7 @@ int main(int argc, char **argv, char **env)
   }
 
   std::stringstream path;
-  path << "example-data/multigroup-data/node" << nodeID;
+  path << "example-data/ondisk-data/node" << nodeID;
   dragonboat::NodeHostConfig nhconfig(path.str(), path.str());
   nhconfig.RTTMillisecond = dragonboat::Milliseconds(200);
   nhconfig.RaftAddress = address;
@@ -100,51 +135,61 @@ int main(int argc, char **argv, char **env)
     return -1;
   }
 
+  printUsage();
+  auto statusAssert = [](const std::string &desc, dragonboat::Status &s)
+  {
+    if (!s.OK()) {
+      std::cerr
+        << desc << " failed, error("
+        << s.Code() << "): " << s.String() << std::endl;
+    }
+  };
   auto timeout = dragonboat::Milliseconds(3000);
+  std::unique_ptr<dragonboat::Session> session(nh->GetNoOPSession(ClusterID));
+  dragonboat::Buffer result(1024);
   for (std::string message; std::getline(std::cin, message);) {
-    auto parts = split(message);
-    if (parts.size() < 1 || parts.size() > 3) {
-      std::cerr << "undefined command: " << message << std::endl;
-      continue;
-    }
-    if (parts.size() == 3 && parts[0] != "put") {
-      std::cerr << "Usage: put key value" << std::endl;
-      continue;
-    }
-    if (parts.size() == 2 && parts[0] != "get") {
-      std::cerr << "Usage: get key" << std::endl;
-      continue;
-    }
-    if (parts[0] == "exit") {
-      break;
-    }
-    dragonboat::Buffer result(1024);
-    switch (parts.size()) {
-      case 2: {
-        dragonboat::Buffer query(
-          reinterpret_cast<const dragonboat::Byte *>(parts[1].c_str()),
-          parts[1].size());
-        status = nh->SyncRead(ClusterID, query, &result, timeout);
+    auto request = parseRequest(message);
+    auto &type = std::get<0>(request);
+    auto &key = std::get<1>(request);
+    auto &value = std::get<2>(request);
+    switch (type) {
+      case EXIT: {
         break;
       }
-      case 3: {
-        auto msg = parts[1] + " " + parts[2];
+      case PUT: {
         dragonboat::Buffer query(
-          reinterpret_cast<const dragonboat::Byte *>(msg.c_str()),
-          msg.size());
-        std::unique_ptr<dragonboat::Session> session(
-          nh->GetNoOPSession(ClusterID));
+          reinterpret_cast<const dragonboat::Byte *>(key.c_str()),
+          key.size());
         dragonboat::UpdateResult ret;
         status = nh->SyncPropose(session.get(), query, timeout, &ret);
         break;
       }
-      default:std::cerr << "error" << std::endl;
+      case GET: {
+        dragonboat::Buffer query(
+          reinterpret_cast<const dragonboat::Byte *>(key.c_str()),
+          key.size());
+        status = nh->SyncRead(ClusterID, query, &result, timeout);
+        break;
+      }
+      case ADD_NODE: {
+        status =
+          nh->SyncRequestAddNode(ClusterID, std::stoi(value), key, timeout);
+        break;
+      }
+      case REMOVE_NODE: {
+        status = nh->SyncRequestDeleteNode(ClusterID, std::stoi(key), timeout);
+        break;
+      }
+      case UNKNOWN: {
+        printUsage();
+        break;
+      }
+      default: {
+        std::cerr << "Fatal" << std::endl;
+        break;
+      }
     }
-    if (status.OK() && result.Len() != 0) {
-      std::cout << result.Data() << std::endl;
-    } else if (!status.OK()) {
-      std::cerr << "error code: " << status.Code() << std::endl;
-    }
+    statusAssert(message, status);
   }
   nh->Stop();
 }
